4665fa28d0ab211c4206e743081a8b659c267781
Dan Volosnikov <myrovoy@mail.ru>
2017-12-29 20:23:35 -0500

    prepared for heroku

--- server.js
+++ server.js
@@ -36,16 +36,16 @@ const port = process.env.PORT || 5000;
 
 
 // Priority serve any static files.
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+server.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Answer API requests.
-app.get('/api', function (req, res) {
+server.get('/api', function (req, res) {
   res.set('Content-Type', 'application/json');
   res.send('{"message":"Hello from the custom server!"}');
 });
 
 // All remaining requests return the React app, so it can handle routing.
-app.get('*', function(request, response) {
+server.get('*', function(request, response) {
   response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
