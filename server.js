568dc727b0c1b849df7eaebaeaedd3b57fdaf9f7
Dan Volosnikov <myrovoy@mail.ru>
2017-12-29 21:03:26 -0500

    prepared for heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
+const path = require('path');
 const cors = require('cors');
 
 const server  = express();
