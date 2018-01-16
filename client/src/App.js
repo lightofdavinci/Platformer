72f0b409e90d9869bf578566b0dd669fe4c8e9c6
Dan Volosnikov <danvolosnikov@gmail.com>
2018-01-16 13:22:11 -0500

    made component stateless

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import * as Pages from './pages';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Switch>
-          <Route exact path="/" component={Pages.Landing} />
-          <Route path="/register" component={Pages.CreateAccount} />
-          <Route path="/login" component={Pages.Login} />
-          <Route path="/game" component={Pages.Game} />
-          <Route path="/leaderboard" component={Pages.Leaderboard} />
-          <Route path="/help" component={Pages.Help} />
-          <Route component={Pages.NotFound} />
-        </Switch>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default () => {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={Pages.Landing} />
+        <Route path="/register" component={Pages.CreateAccount} />
+        <Route path="/login" component={Pages.Login} />
+        <Route path="/game" component={Pages.Game} />
+        <Route path="/leaderboard" component={Pages.Leaderboard} />
+        <Route path="/help" component={Pages.Help} />
+        <Route component={Pages.NotFound} />
+      </Switch>
+    </div>
+  );
+};
